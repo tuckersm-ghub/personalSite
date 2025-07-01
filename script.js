410cf9f6030af2a8ff5e2fcf54b7563ca9acf80c
tuckersm-ghub <shanemtucker05@gmail.com>
2025-07-01 15:02:42 -0400

    Achievements.html -> resume.html, resume page completed, began ? implementation

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-
-function toHomePage(){
-  let btnClick = document.getElementById("homeBTN");
-  btnClick.addEventListener("click", () => {window.location.href = "index.html"});
-}
-
-function toAboutPage(){
-  let btnClick= document.getElementById("aboutBTN");
-  btnClick.addEventListener("click", () => {window.location.href = "about.html"});
-}
-
-function toAchievementsPage(){
-  let btnClick = document.getElementById("achievementsBTN");
-  btnClick.addEventListener("click", () => {window.location.href = "achievements.html"});
-}
+loadPageWithHeader(); 
 
 function loadPageWithHeader() {
   if(!document.querySelector('header')) {
@@ -26,17 +12,68 @@ function loadPageWithHeader() {
                   document.body.insertAdjacentElement('afterbegin', headerElement);
                   toHomePage();
                   toAboutPage();
-                  toAchievementsPage();
+                  toResumePage();
+                  questionMark();
               }
 
           })
           .catch(error => console.error('Failed to fetch header:', error));
   }
-  
-  toHomePage();
-  toAboutPage();
-  toAchievementsPage();
+  else{
+    toHomePage();
+    toAboutPage();
+    toResumePage();
+    questionMark();
+  }
+}
+
+function exclamationMark(){
+  let btnClick = document.getElementById("!BTN");
+  btnClick.addEventListener("click", () => {})
+}
+
+function toHomePage(){
+  let btnClick = document.getElementById("homeBTN");
+  btnClick.addEventListener("click", () => {window.location.href = "index.html"});
+}
+
+function toAboutPage(){
+  let btnClick= document.getElementById("aboutBTN");
+  btnClick.addEventListener("click", () => {window.location.href = "about.html"});
+}
+
+function toResumePage(){
+  let btnClick = document.getElementById("resumeBTN");
+  btnClick.addEventListener("click", () => {window.location.href = "resume.html"});
+}
+
+function questionMark(){
+  let btnClick = document.getElementById("?BTN");
+  btnClick.addEventListener("click", () => {
+    if(window.location.href.includes("index.html")){
+      randomShape(document.getElementById("design1"), document.getElementById("design2a"), document.getElementById("design2b"));
+    }
+    else if(window.location.href.includes("about.html")){
+      
+    }
+    else if(window.location.href.includes("resume.html")){
+      
+    }
+  })
 }
 
 
-loadPageWithHeader(); 
+
+function randomShape(leftElement, rightElement1, rightElement2){
+  leftElement.style.width = (Math.random() * 50 + 200) + "px";
+  leftElement.style.height = (Math.random() * 50 + 200) + "px";
+  leftElement.style.borderRadius = (Math.random() * 30 + 25) + "%";
+
+  rightElement1.style.width = (Math.random() * 50 + 10) + "px";
+  rightElement1.style.height = (Math.random() * 50 + 200) + "px";
+  rightElement1.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  rightElement2.style.width = (Math.random() * 50 + 10) + "px";
+  rightElement2.style.height = (Math.random() * 50 + 200) + "px";
+  rightElement2.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
